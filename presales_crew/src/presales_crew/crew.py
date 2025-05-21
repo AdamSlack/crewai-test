a090645f036ea459b605c5d8872ee29560a6a654
adam slack <adam.slack@nimbleapproach.com>
2025-05-21 08:13:04 +0100

    write to PDF

--- presales_crew/src/presales_crew/crew.py
+++ presales_crew/src/presales_crew/crew.py
@@ -3,16 +3,29 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
 from crewai.knowledge.source.pdf_knowledge_source import PDFKnowledgeSource
+from crewai_tools import SerperDevTool
+from markdown_pdf import MarkdownPdf, Section
+
+
 import os
 
-llm = LLM(model="gpt-4o-mini", temperature=0)
+llm = LLM(model="gpt-4o", temperature=0)
 
+require_human_input = False
 
 def save_output(output: TaskOutput):
-    file_path = f"outputs/{output.agent.lower().replace(' ', '_')}.md"
-    os.makedirs("outputs", exist_ok=True)
+    file_name = output.agent.lower().replace(' ', '_')
+    file_path = f"outputs/markdown/{file_name}.md"
+    os.makedirs("outputs/markdown", exist_ok=True)
+    os.makedirs("outputs/pdf", exist_ok=True)
     with open(file_path, "w") as f:
         f.write(output.raw)
+    # use markdown-pdf to convert the output to a pdf
+    pdf = MarkdownPdf(toc_level=2, optimize=True)
+    with open(file_path, "r") as f:
+        markdown_content = f.read()
+    pdf.add_section(Section(markdown_content))
+    pdf.save(f"outputs/pdf/{file_name}.pdf")
 
 @CrewBase
 class PresalesCrew():
@@ -22,15 +35,28 @@ class PresalesCrew():
     tasks: List[Task]
 
     pdf_source = PDFKnowledgeSource(
+            # file_paths=["../knowledge/AI Scan Education Documents Brief 010525.pdf"] # Update this path to your actual PDF
             file_paths=["../knowledge/Client Brief_ Performance and Progression Review Software Solution.pdf"] # Update this path to your actual PDF
         )
 
+    #
+    # Agents
+    #
     @agent
     def brief_interpreter(self) -> Agent:
         return Agent(
             config=self.agents_config['brief_interpreter'], # type: ignore[index]
             verbose=True,
             knowledge_sources=[self.pdf_source]
+        )
+    
+    @agent
+    def comparative_product_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['comparative_product_researcher'], # type: ignore[index]
+            verbose=True,
+            tools=[SerperDevTool()],
+            knowledge_sources=[self.pdf_source],
         )
     
     @agent
@@ -62,6 +88,7 @@ class PresalesCrew():
         return Agent(
             config=self.agents_config['slide_creator'], # type: ignore[index]
             verbose=True,
+            knowledge_sources=[self.pdf_source],
         )
 
     @agent
@@ -69,22 +96,34 @@ class PresalesCrew():
         return Agent(
             config=self.agents_config['user_journey_mapper'], # type: ignore[index]
             verbose=True,
+            knowledge_sources=[self.pdf_source],
         )
 
+    #
+    # Tasks
+    #
     @task
     def brief_interpretation_task(self) -> Task:
         return Task(
             config=self.tasks_config['brief_interpretation_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
 
+    @task
+    def market_comparison_research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['market_comparison_research_task'], # type: ignore[index]
+            callback=save_output,
+            human_input=require_human_input
+        )
+    
     @task
     def user_persona_generation_task(self) -> Task:
         return Task(
             config=self.tasks_config['user_persona_generation_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
 
     @task
@@ -92,7 +131,7 @@ class PresalesCrew():
         return Task(
             config=self.tasks_config['gap_analysis_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
     
     @task
@@ -100,7 +139,7 @@ class PresalesCrew():
         return Task(
             config=self.tasks_config['epic_story_generation_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
     
     @task
@@ -108,7 +147,7 @@ class PresalesCrew():
         return Task(
             config=self.tasks_config['user_journey_mapping_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
     
     @task
@@ -116,10 +155,9 @@ class PresalesCrew():
         return Task(
             config=self.tasks_config['slide_deck_generation_task'], # type: ignore[index]
             callback=save_output,
-            human_input=True
+            human_input=require_human_input
         )
     
-
     @crew
     def crew(self) -> Crew:
         """Creates the PresalesCrew crew"""
--- presales_crew/src/presales_crew/main.py
+++ presales_crew/src/presales_crew/main.py
@@ -2,25 +2,15 @@
 import sys
 import warnings
 
-from datetime import datetime
-
 from presales_crew.crew import PresalesCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 def run():
     """
     Run the crew.
     """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
+    inputs = {}
     
     try:
         PresalesCrew().crew().kickoff(inputs=inputs)
@@ -32,10 +22,7 @@ def train():
     """
     Train the crew for a given number of iterations.
     """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
+    inputs = {}
     try:
         PresalesCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
@@ -56,10 +43,7 @@ def test():
     """
     Test the crew execution and returns the results.
     """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
+    inputs = {}
     
     try:
         PresalesCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
